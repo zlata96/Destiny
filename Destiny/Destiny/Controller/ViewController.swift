784209e647f20412ef5d98c323b38ca4b4bf1ff2
Zlata Guseva <gusevazlata96@gmail.com>
2022-08-30 23:50:32 +0400

    Add model

--- Destiny/Destiny/Controller/ViewController.swift
+++ Destiny/Destiny/Controller/ViewController.swift
@@ -14,11 +14,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    let stories = [Story(title: "You see a fork in the road", choice1: "Take a left.", choice2: "Take a right."), Story(title: "You see a tiger", choice1: "Shout for help.", choice2: "Play dead."), Story(title: "You find a treasure chest", choice1: "Open it.", choice2: "Check for traps.")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        storyLabel.text = stories[0].title
+        choice1Button.setTitle(stories[0].choice1, for: .normal)
+        choice2Button.setTitle(stories[0].choice2, for: .normal)
     }
-
 
 }
 
