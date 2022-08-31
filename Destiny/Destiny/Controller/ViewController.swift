0e95ad2bd9a5113ca69bb91dd01feaa0281d27e9
Zlata Guseva <gusevazlata96@gmail.com>
2022-08-31 23:51:53 +0400

    Refactoring

--- Destiny/Destiny/Controller/ViewController.swift
+++ Destiny/Destiny/Controller/ViewController.swift
@@ -14,14 +14,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    let stories = [Story(title: "You see a fork in the road", choice1: "Take a left.", choice2: "Take a right."), Story(title: "You see a tiger", choice1: "Shout for help.", choice2: "Play dead."), Story(title: "You find a treasure chest", choice1: "Open it.", choice2: "Check for traps.")]
-    
+    var storyBrain = StoryBrain()
+      
     override func viewDidLoad() {
         super.viewDidLoad()
-        storyLabel.text = stories[0].title
-        choice1Button.setTitle(stories[0].choice1, for: .normal)
-        choice2Button.setTitle(stories[0].choice2, for: .normal)
+        updateUI()
     }
 
+    @IBAction func choiceMade(_ sender: UIButton) {
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+        updateUI()
+    }
+    
+    func updateUI() {
+        
+        storyLabel.text = storyBrain.stories[storyBrain.storyNumber].title
+        choice1Button.setTitle(storyBrain.stories[storyBrain.storyNumber].choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.stories[storyBrain.storyNumber].choice2, for: .normal)
+    }
 }
 
--- Destiny/Destiny/Model/Story.swift
+++ Destiny/Destiny/Model/Story.swift
@@ -11,5 +11,7 @@ import Foundation
 struct Story {
     var title: String
     var choice1: String
-    var choice2: String 
+    var choice1Destination: Int
+    var choice2: String
+    var choice2Destination: Int
 }
